6c82bd4fba1b774968869873aa65e12ee72086a0
Ben <eklundbenjaminj@gmail.com>
2024-05-30 17:13:57 -0700

    meh

--- Game/src/Level/LevelOne.cpp
+++ Game/src/Level/LevelOne.cpp
@@ -8,9 +8,9 @@
 
 std::unique_ptr<GameStateManager> LevelOne::GetGameState() {
     auto player = std::make_unique<Player>();
-    auto scene = std::make_unique<GameStateManager>();
+    auto gameState = std::make_unique<GameStateManager>();
 
-    scene->AddObject(std::move(player));
+    gameState->AddObject(std::move(player));
 
     auto platform1 = std::make_unique<Platform>(0, (float)GetScreenHeight() - 100, (float)GetScreenWidth(), 100, DARKBROWN);
     auto platform2 = std::make_unique<Platform>(550, (float)GetScreenHeight() - 400, 500, 50, BLACK);
@@ -18,12 +18,12 @@ std::unique_ptr<GameStateManager> LevelOne::GetGameState() {
     auto platform4 = std::make_unique<Platform>(550, 300, 500, 50, BLACK);
 
     auto tile = std::make_unique<Tile>(100, 100);
-    scene->AddObject(std::move(tile));
+    gameState->AddObject(std::move(tile));
 
-    scene->AddObject(std::move(platform1));
-    scene->AddObject(std::move(platform2));
-    scene->AddObject(std::move(platform3));
-    scene->AddObject(std::move(platform4));
+    gameState->AddObject(std::move(platform1));
+    gameState->AddObject(std::move(platform2));
+    gameState->AddObject(std::move(platform3));
+    gameState->AddObject(std::move(platform4));
 
     auto monster1 = std::make_unique<Boar>(1000, 200, 100, 100, MonsterState::PACING);
     auto monster2 = std::make_unique<Boar>(2000, GetScreenHeight() - 200, 100, 100, MonsterState::DEFAULT);
@@ -36,8 +36,8 @@ std::unique_ptr<GameStateManager> LevelOne::GetGameState() {
     }
      */
 
-    scene->AddObject(std::move(monster1));
-    scene->AddObject(std::move(monster2));
+    gameState->AddObject(std::move(monster1));
+    gameState->AddObject(std::move(monster2));
 
-    return scene;
+    return gameState;
 }
