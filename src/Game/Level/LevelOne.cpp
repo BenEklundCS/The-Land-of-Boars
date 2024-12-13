cce79c2a68e11f553aeb149d9b5c10b7b6835b6f
BenEklundCS <eklundbenjaminj@gmail.com>
2024-12-12 17:47:47 -0800

    level moved around back to account for alignment of tilemanager with origin

--- src/Game/Level/LevelOne.cpp
+++ src/Game/Level/LevelOne.cpp
@@ -25,14 +25,14 @@ GameStateManager* LevelOne::GetGameState() {
 
 #pragma region platforms
     // Spawn platforms into the gameState with absolute positions
-    auto platform3 = std::make_unique<Platform>(3200 - origin.x, 700 - origin.y, 500, 25, BLACK);
-    auto moving_platform = std::make_unique<MovingPlatform>(std::move(platform3), Vector2{0, 0}, Vector2{-700, 700}, 500.0f, false, true);
-    auto platform4 = std::make_unique<Platform>(550 - origin.x, 400 - origin.y, 500, 50, BLACK);
+    auto platform3 = std::make_unique<Platform>(3-50, 700, 250, 25, BLACK);
+    auto moving_platform = std::make_unique<MovingPlatform>(std::move(platform3), Vector2{0, 0}, Vector2{-700, 700}, 250.0f, false, true);
+    auto platform4 = std::make_unique<Platform>(550, 400, 500, 50, BLACK);
     auto moving_platform2 = std::make_unique<MovingPlatform>(std::move(platform4), Vector2{-200, 200}, Vector2{0, 0}, 200.0f, true, false);
 
     // Adding additional platforms with consistent spacing
     for (int i = 0; i < 4; i++) {
-        auto p = std::make_unique<Platform>(550 + i * 1000 - origin.x, 600 - i * 300 - origin.y, 500, 50, BLACK);
+        auto p = std::make_unique<Platform>(550 + i * -19000, 600 - i * 300, 500, 50, BLACK);
         gameState->AddObject(std::move(p));
     }
 
@@ -43,7 +43,7 @@ GameStateManager* LevelOne::GetGameState() {
 
 #pragma region game tiles
     // Get the TileManager
-    auto tileManager = std::make_unique<TileManager>(Vector2{origin.x, origin.y});
+    auto tileManager = std::make_unique<TileManager>(Vector2{-2000, 250});
     // Get the LevelLoader and use it to make a tileMap to load
     auto levelLoader = std::make_unique<LevelLoader>();
     auto tileMap = levelLoader->LoadLevel("../Levels/map_level_one.txt");
@@ -53,30 +53,32 @@ GameStateManager* LevelOne::GetGameState() {
 
 #pragma region monsters
     // Spawn in and add monsters to the game
-    auto monster1 = std::make_unique<Boar>(1000 - origin.x, 200 - origin.y, 300, 300, MonsterState::DEFAULT);
-    auto monster2 = std::make_unique<Boar>(2000 - origin.x, 500 - origin.y, 100, 100, MonsterState::DEFAULT);
+    auto monster1 = std::make_unique<Boar>(1000, 200, 300, 300, MonsterState::DEFAULT);
+    auto monster2 = std::make_unique<Boar>(2000, 500, 100, 100, MonsterState::DEFAULT);
 
     gameState->AddObject(std::move(monster1));
     gameState->AddObject(std::move(monster2));
 
+    /*
     // Add additional boars in a line
     for (int i = 0; i < 10; i++) {
-        auto monster3 = std::make_unique<Boar>(2000 + i * 50 - origin.x, 500 - origin.y, 100, 100, MonsterState::DEFAULT);
+        auto monster3 = std::make_unique<Boar>(0 + i * 50, 500, 100, 100, MonsterState::DEFAULT);
         gameState->AddObject(std::move(monster3));
     }
+    */
 
     // Bee experiment
     auto bee = std::make_unique<Bee>(100, 100, 300, 300, MonsterState::DEFAULT);
     gameState->AddObject(std::move(bee));
 
     // Snail
-    auto snail = std::make_unique<Snail>(1500 - origin.x, 300 - origin.y, 100, 100, MonsterState::DEFAULT);
+    auto snail = std::make_unique<Snail>(1500, 300, 100, 100, MonsterState::DEFAULT);
     gameState->AddObject(std::move(snail));
 
 #pragma endregion
 
 #pragma region trees
-    auto tree = std::make_unique<Tree>(2500 - origin.x, 375 - origin.y, GREEN_TREE_TEXTURE);
+    auto tree = std::make_unique<Tree>(2500, 375, GREEN_TREE_TEXTURE);
     gameState->AddObject(std::move(tree));
 #pragma endregion
 
